c9d919de1831e6fa8446b6571b78a6bc81c8ce76
SebastianG03 <dasedev24@gmail.com>
2025-11-22 09:12:03 -0500

    sql queries deleted for clean database creation

--- init_jugadores.sql
+++ init_jugadores.sql
@@ -1,13 +1,13 @@
 CREATE DATABASE udlafutbolappestudiantes;
 
-\connect udlafutbolappestudiantes;
+-- \connect udlafutbolappestudiantes;
 
-CREATE TABLE IF NOT EXISTS jugador (
-    idjugador SERIAL PRIMARY KEY,
-    idbanner VARCHAR(9) NOT NULL,
-    nombrejugador VARCHAR(250) NOT NULL,
-    apellidojugador VARCHAR(250) NOT NULL,
-    numerocamisetajugador INT NOT NULL,
-    posicionjugador VARCHAR(250) NOT NULL,
-    jugadoractivo BOOLEAN NOT NULL
-);
+-- CREATE TABLE IF NOT EXISTS jugador (
+--     idjugador SERIAL PRIMARY KEY,
+--     idbanner VARCHAR(9) NOT NULL,
+--     nombrejugador VARCHAR(250) NOT NULL,
+--     apellidojugador VARCHAR(250) NOT NULL,
+--     numerocamisetajugador INT NOT NULL,
+--     posicionjugador VARCHAR(250) NOT NULL,
+--     jugadoractivo BOOLEAN NOT NULL
+-- );
